217c37298298a180f15ee95eefc0a3601015e6db
Anton Lamtev <anton.lamtev@corp.mail.ru>
2018-09-26 00:33:07 +0300

    refactoring, remove shitty mutex code, add NOSIGPIPE option

--- tcp/client/main.c
+++ tcp/client/main.c
@@ -6,16 +6,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <nets_lib/receivenbytes.h>
+
 int main(int argc, char **argv) {
     char *message;
     if (argc == 1) {
-        message = "1234567_";
+        message = "default message default message default message default message default message";
     } else {
         message = argv[1];
     }
     struct sockaddr_in peer;
     peer.sin_family = AF_INET;
-    peer.sin_port = htons(7500);
+    peer.sin_port = htons(1234);
     peer.sin_addr.s_addr = inet_addr("127.0.0.1");
 
     int _socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -29,18 +31,20 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    for (int i = 0; i < 8; ++i) {
+    unsigned long long x = 100L;
+    while (x--) {
         if (send(_socket, message, strlen(message), 0) <= 0) {
             perror("Unable to send");
             return 1;
         }
-    }
 
-    char buf[255];
-    if (recv(_socket, buf, 255, 0) <= 0) {
-        perror("Unable to recv");
-    } else {
-        printf("%s\n", buf);
+        char buf[255];
+        long received = receiveNBytes(_socket, 32, buf);
+        if (received == 0 || received == -1) {
+            perror("Unable to recv");
+        } else {
+            printf("%s\n", buf);
+        }
     }
 
     return 0;
