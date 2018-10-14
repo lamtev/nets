b57b4e32a558922a3e4596bcb454867576a24c8e
Anton Lamtev <anton.lamtev@corp.mail.ru>
2018-10-14 14:04:19 +0300

    changes after @vaddya review

--- nets_lib/send.cpp
+++ nets_lib/send.cpp
@@ -11,7 +11,7 @@ ssize_t send(int socket, void *bytes, size_t size) {
 #ifdef __APPLE__
     return ::send(socket, bytes, size, 0);
 #else
-    return send(socket, bytes, result.size(), MSG_NOSIGNAL);
+    return ::send(socket, bytes, result.size(), MSG_NOSIGNAL);
 #endif
 }
 
